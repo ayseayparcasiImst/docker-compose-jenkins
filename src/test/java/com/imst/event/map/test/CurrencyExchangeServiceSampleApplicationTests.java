37919a61c5ca689df17c6cc4a887b04d8bb3e829
Ayşe Ayparçası <ayse.ayparcasi@imst.com.tr>
2023-07-14 16:17:29 +0300

    test ContextConfiguration eklendi çakışma oldu

--- src/test/java/com/imst/event/map/test/CurrencyExchangeServiceSampleApplicationTests.java
+++ src/test/java/com/imst/event/map/test/CurrencyExchangeServiceSampleApplicationTests.java
@@ -2,8 +2,10 @@ package com.imst.event.map.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes=CurrencyExchangeServiceSampleApplicationTests.class)
 class CurrencyExchangeServiceSampleApplicationTests {
 
 	@Test
